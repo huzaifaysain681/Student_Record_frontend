f21efda70c7431d293e75019f02fa1a5cd9b68a8
huzaifa <huzaifa@kali.kali>
2024-04-16 11:05:59 +0500

    Frontend On Vercel

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
-const backendUrl = 'https://hilarious-leg-warmers-lion.cyclic.app';
+const backendUrl = 'https://backend-j0xk8j3w3-huzaifaysain681s-projects.vercel.app';
 
 export default backendUrl;
