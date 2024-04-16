2b8edeab918c26e70b103ce5feb417dc755a04f6
huzaifa <huzaifa@kali.kali>
2024-04-16 11:38:17 +0500

    Frontend Record

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
-const backendUrl = 'https://backend-gurhgy8yk-huzaifaysain681s-projects.vercel.app';
+const backendUrl = 'https://backend-5ncghxnm8-huzaifaysain681s-projects.vercel.app';
 
 export default backendUrl;
